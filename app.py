47e2e7b813888c365e9ce85ac32b0e40e3723723
charuay05 <57581355+charuay05@users.noreply.github.com>
2020-08-03 23:54:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ import os
 app = Flask(__name__)
 
 color_codes = {
-  "red": "#e74c3c",
-  "green": "#16a085",
-  "blue": "#2980b9",
-  "blue2": "#30336b",
-  "pink": "#be2edd",
-  "darkblue": "#130f40"
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "blue2": "#30336b",
+    "pink": "#be2edd",
+    "darkblue": "#130f40"
 }
- 
+
 color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
 
 @app.route("/")
@@ -33,6 +33,6 @@ def read_file():
     f = open("/data/testfile.txt")
     contents = f.read()
     return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-    
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port="8080")
